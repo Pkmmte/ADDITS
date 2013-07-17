bca6854d8b6a1849b0ff344ca1b722858f7a3834
Pkmmte <pkx7225@gmail.com>
2013-07-16 22:00:48 -0700

    And This

--- ADDITS/src/com/pk/addits/Data.java
+++ ADDITS/src/com/pk/addits/Data.java
@@ -612,39 +612,6 @@ public class Data
 		return contentList;
 	}
 	
-	public static String getInnerXml(XmlPullParser parser) throws XmlPullParserException, IOException
-	{
-		StringBuilder sb = new StringBuilder();
-		int depth = 1;
-		while (depth != 0)
-		{
-			switch (parser.next())
-			{
-				case XmlPullParser.END_TAG:
-					depth--;
-					if (depth > 0)
-					{
-						sb.append("</" + parser.getName() + ">");
-					}
-					break;
-				case XmlPullParser.START_TAG:
-					depth++;
-					StringBuilder attrs = new StringBuilder();
-					for (int i = 0; i < parser.getAttributeCount(); i++)
-					{
-						attrs.append(parser.getAttributeName(i) + "=\"" + parser.getAttributeValue(i) + "\" ");
-					}
-					sb.append("<" + parser.getName() + " " + attrs.toString() + ">");
-					break;
-				default:
-					sb.append(parser.getText());
-					break;
-			}
-		}
-		String content = sb.toString();
-		return content;
-	}
-	
 	public static String pullLinks(String text)
 	{
 		ArrayList<String> links = new ArrayList<String>();
