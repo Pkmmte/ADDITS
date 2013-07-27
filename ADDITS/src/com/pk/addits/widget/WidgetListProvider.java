2f5c362dedf600246ec5e373215da9cce991038f
Pkmmte <pkx7225@gmail.com>
2013-07-26 21:12:57 -0700

    Clickable WidgetList

--- ADDITS/src/com/pk/addits/widget/WidgetListProvider.java
+++ ADDITS/src/com/pk/addits/widget/WidgetListProvider.java
@@ -15,6 +15,8 @@ import com.pk.addits.R;
 public class WidgetListProvider extends AppWidgetProvider
 {
 	public static String EXTRA_ID = "com.pk.addits.widget.EXTRA_ID";
+	public static final String ARTICLE_ACTION = "com.pk.addits.widget.ARTICLE_ACTION";
+	public static final String REFRESH_ACTION = "com.pk.addits.widget.REFRESH_ACTION";
 	
 	@Override
 	public void onEnabled(Context context)
@@ -28,10 +30,18 @@ public class WidgetListProvider extends AppWidgetProvider
 	public void onReceive(Context context, Intent intent)
 	{
 		super.onReceive(context, intent);
-		String strAction = intent.getAction();
-		if (AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(strAction))
+		
+		if (intent.getAction().equals(REFRESH_ACTION))
 		{
 			/* Do update */
+		}
+		else if(intent.getAction().equals(ARTICLE_ACTION))
+		{
+			int viewID = intent.getIntExtra(EXTRA_ID, 1);
+			Intent articleIntent = new Intent(context, ActivityMain.class);
+			articleIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			articleIntent.putExtra(EXTRA_ID, viewID);
+			context.startActivity(articleIntent);
 		}
 	}
 	
@@ -49,10 +59,17 @@ public class WidgetListProvider extends AppWidgetProvider
 			
 			widget.setRemoteAdapter(R.id.articles, svcIntent);
 			
-			Intent clickIntent = new Intent(context, ActivityMain.class);
-			PendingIntent clickPI = PendingIntent.getActivity(context, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+			//Intent clickIntent = new Intent(context, ActivityMain.class);
+			//PendingIntent clickPI = PendingIntent.getActivity(context, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 			
+			//widget.setPendingIntentTemplate(R.id.articles, clickPI);
+			
+			Intent clickIntent = new Intent(context, WidgetListProvider.class);
+			clickIntent.setAction(WidgetListProvider.ARTICLE_ACTION);
+			clickIntent.setData(Uri.parse(clickIntent.toUri(Intent.URI_INTENT_SCHEME)));
+			PendingIntent clickPI = PendingIntent.getBroadcast(context, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 			widget.setPendingIntentTemplate(R.id.articles, clickPI);
+			
 			
 			appWidgetManager.updateAppWidget(appWidgetIds[i], widget);
 		}
