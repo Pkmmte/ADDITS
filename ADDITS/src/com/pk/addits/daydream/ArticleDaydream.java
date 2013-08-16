3209c7fb496b0619ba79d091d8f200c584a7b85b
Pkmmte <pkx7225@gmail.com>
2013-08-15 17:49:31 -0700

    Set up basic daydream

---
+++ ADDITS/src/com/pk/addits/daydream/ArticleDaydream.java
@@ -0,0 +1,38 @@
+package com.pk.addits.daydream;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.service.dreams.DreamService;
+
+import com.pk.addits.R;
+
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+public class ArticleDaydream extends DreamService
+{
+	private Context context;
+	
+	@Override
+	public void onDreamingStarted()
+	{
+		// daydream started
+		context = getApplicationContext();
+	}
+	
+	@Override
+	public void onDreamingStopped()
+	{
+		// daydream stopped
+	}
+	
+	@Override
+	public void onAttachedToWindow()
+	{
+		
+		super.onAttachedToWindow();
+		setInteractive(true);
+		setFullscreen(true);
+		setContentView(R.layout.daydream_article);
+		
+	}
+}
