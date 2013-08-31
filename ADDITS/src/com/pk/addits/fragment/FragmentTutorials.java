9ad753eec1a63d078b74f66b2ca40fc0b056d69b
adnan-SM <adnan@screen-magic.com>
2013-08-31 10:59:24 +0530

    Added Google Analytics

--- ADDITS/src/com/pk/addits/fragment/FragmentTutorials.java
+++ ADDITS/src/com/pk/addits/fragment/FragmentTutorials.java
@@ -6,6 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.Fields;
+import com.google.analytics.tracking.android.MapBuilder;
+import com.google.analytics.tracking.android.Tracker;
+
 import com.pk.addits.R;
 
 public class FragmentTutorials extends Fragment
@@ -22,5 +27,15 @@ public class FragmentTutorials extends Fragment
 	public void onStart()
 	{
 		super.onStart();
+		Tracker easyTracker = EasyTracker.getInstance(getActivity());
+
+		// This screen name value will remain set on the tracker and sent with
+		// hits until it is set to a new value or to null.
+		easyTracker.set(Fields.SCREEN_NAME, "Tutorials Screen");
+
+		easyTracker.send(MapBuilder
+		    .createAppView()
+		    .build()
+		);
 	}
 }
